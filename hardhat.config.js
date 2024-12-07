2bcb28f01815e6dd5e098e915cdd14d09ae43473
Ivan Carneiro <ivanexequielc@gmail.com>
2024-12-07 13:32:19 -0300

    deploy & verify to ETH Sepolia OK

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
+
+const ALCHEMY_URL = process.env.ALCHEMY_URL;
+const ETHERSCAN_APIKEY = process.env.ETHERSCAN_APIKEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.27",
+    solidity: "0.8.27",
+    networks: {
+        sepolia: {
+            url: ALCHEMY_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_APIKEY,
+    },
+    sourcify: {
+        enabled: true,
+    }
 };
